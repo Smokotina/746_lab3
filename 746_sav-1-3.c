ba253d48c8a898bca1bcc32d6bab55b228ca364c
Смокотина <746_sav@timp2018.novalocal>
2018-05-28 13:20:42 +0000

    Первая практика

---
+++ 746_sav-1-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+double a,b,s;
+int main ()
+{
+printf("Input a,b:\n");
+scanf("%lf" "%lf", &a,&b);
+s=pow(a,b);
+printf ("%lf", s);
+printf("\n");
+return 0;
+}
